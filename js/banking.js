1a712a003dd9ff1183249c20548e36c31918b4d4
hasanabidruposh <hasan.ruposh@gmail.com>
2021-08-17 00:49:44 +0600

    third commit

--- js/banking.js
+++ js/banking.js
@@ -1,73 +1,70 @@
-/*
-function doubleIt(num) {
-    const result = num * 2;
-    return result;
-}
-
-const first = doubleIt(5); //5*2=10
-const second = doubleIt(7); //7*2=14
-*/
+/* function_1 for getting both 
+deposite input & withdraw input Amount */
 
 function getInputValue(inputId) {
-    const depositeInput = document.getElementById(inputId);
-    const depositeAmountText = depositeInput.value;
-    const depositeAmount = parseFloat(depositeAmountText);
+    const userInput = document.getElementById(inputId);
+    const userInputText = userInput.value;
+    const userInputAmount = parseFloat(userInputText);
     //Clear input field
-    depositeInput.value = '';
-    return depositeAmount;
+    userInput.value = '';
+    return userInputAmount;
 
 }
 
-
-//  console.log();
-//get deposite input Amount
-document.getElementById('deposite-button').addEventListener('click', function () {
-    /*  const depositeInput = document.getElementById('deposite-input');
-     const depositeAmount = parseFloat(depositeInput.value); */
-
-    //function call
-    const depositeAmount = getInputValue('deposite-input');
-
+// function_2 for getting both total deposite and total withdraw 
+function updateTotalField(totalFieldId, inputAmount) {
+    // debugger;
     //get current deposite
-    const depositeTotal = document.getElementById('deposite-amount');
-    const preDepositeTotal = parseFloat(depositeTotal.innerText);
-
+    const totalField = document.getElementById(totalFieldId);
+    const pretotal = parseFloat(totalField.innerText);
     //get updated deposite
-    depositeTotal.innerText = preDepositeTotal + depositeAmount;
+    totalField.innerText = pretotal + inputAmount;
+}
 
+// function_3 for getting final balance
+function updateBalance(inputAmount, isAdd) {
     //update Balance
     const balanceTotal = document.getElementById('balance-total');
     const preBalanceTotal = parseFloat(balanceTotal.innerText);
 
-    //get updated Balance
-    balanceTotal.innerText = depositeAmount + preBalanceTotal;
+    if (isAdd == true) {
+        //get updated Balance after deposite
+        balanceTotal.innerText = inputAmount + preBalanceTotal;
+    }
+    else {
+        //get updated Balance after withdraw
+        balanceTotal.innerText = preBalanceTotal - inputAmount;
+    }
+}
 
+
+////get deposite input Amount
+document.getElementById('deposite-button').addEventListener('click', function () {
+
+    //function_1 call for getting previous deposite 
+    const depositeAmount = getInputValue('deposite-input');
+
+    //function_2 call for showing updated deposite
+    updateTotalField('deposite-amount', depositeAmount);
+
+    //function_3 call for showing updated Balance
+    updateBalance(depositeAmount, true);
 
 
 });
 
 //handle Withdraw Amount
-//get Withdraw input Amount
+
+////get Withdraw input Amount
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    /* const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmountText = withdrawInput.value;
-    const withdrawAmount = parseFloat(withdrawAmountText); */
 
+    //function_1 call for getting previous withdraw
     const withdrawAmount = getInputValue('withdraw-input');
-    //get current Withdraw
-    const withdrawTotal = document.getElementById('withdraw-amount');
-    const preWithdrawTotal = parseFloat(withdrawTotal.innerText);
 
-    //get updated Withdraw
-    withdrawTotal.innerText = preWithdrawTotal + withdrawAmount;
+    //function_2 call for showing updated withdraw
+    updateTotalField('withdraw-amount', withdrawAmount);
 
-    //get current Balance
-    const balanceTotal = document.getElementById('balance-total');
-    const preBalanceTotal = parseFloat(balanceTotal.innerText);
-
-    //update Balance after Withdraw
-    balanceTotal.innerText = preBalanceTotal - withdrawAmount;
-
-
+    //function_3 call for showing updated Balance
+    updateBalance(withdrawAmount, false);
 });
 
